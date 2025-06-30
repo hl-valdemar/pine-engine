4082bdbb337f2c67cb3ce9cdac6249767e8ca8a7
hl-valdemar <valdemar.lorenzen@gmail.com>
2025-06-30 23:43:37 +0200

    Upgraded to debug allocator

--- src/examples/windows.zig
+++ src/examples/windows.zig
@@ -9,7 +9,7 @@ pub const std_options = std.Options{
 };
 
 pub fn main() !void {
-    var gpa = std.heap.GeneralPurposeAllocator(.{}){};
+    var gpa = std.heap.DebugAllocator(.{}).init;
     const allocator = gpa.allocator();
     defer _ = gpa.deinit();
 
