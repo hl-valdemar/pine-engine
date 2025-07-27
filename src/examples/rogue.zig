378422f4dfe68a636cb27c03dedd9c43c9f30e37
hl-valdemar <valdemar.lorenzen@gmail.com>
2025-07-27 23:14:18 +0200

    Add room walls and basic collision detection

--- src/examples/rogue.zig
+++ src/examples/rogue.zig
@@ -1,4 +1,6 @@
 const std = @import("std");
+const Allocator = std.mem.Allocator;
+
 const pine = @import("pine-engine");
 
 pub const std_options = std.Options{
@@ -24,13 +26,83 @@ pub fn main() !void {
     try app.run();
 }
 
-const Player = struct {};
+const PlayerComponent = struct {};
+const WallComponent = struct {};
 
 const SetupSystem = struct {
+    pub fn init(_: Allocator) anyerror!SetupSystem {
+        return SetupSystem{};
+    }
+
     pub fn process(_: *SetupSystem, registry: *pine.ecs.Registry) anyerror!void {
+        // spawn vertical walls
+        const width = 10;
+        const height = 10;
+        for (2..12) |y| {
+            _ = try registry.spawn(.{
+                WallComponent{},
+                pine.TermPositionComponent{
+                    .x = @intCast(2),
+                    .y = @intCast(y),
+                },
+                pine.TermSpriteComponent{
+                    .symbol = '#',
+                    .color = pine.terminal.TermColor.fromRGB(
+                        pine.terminal.colors.white.rgb,
+                        pine.terminal.colors.black.rgb,
+                    ),
+                },
+            });
+            _ = try registry.spawn(.{
+                WallComponent{},
+                pine.TermPositionComponent{
+                    .x = @intCast(width - 1 + 2),
+                    .y = @intCast(y),
+                },
+                pine.TermSpriteComponent{
+                    .symbol = '#',
+                    .color = pine.terminal.TermColor.fromRGB(
+                        pine.terminal.colors.white.rgb,
+                        pine.terminal.colors.black.rgb,
+                    ),
+                },
+            });
+        }
+        // spawn horizontal walls
+        for (2..12) |x| {
+            _ = try registry.spawn(.{
+                WallComponent{},
+                pine.TermPositionComponent{
+                    .x = @intCast(x),
+                    .y = @intCast(2),
+                },
+                pine.TermSpriteComponent{
+                    .symbol = '#',
+                    .color = pine.terminal.TermColor.fromRGB(
+                        pine.terminal.colors.white.rgb,
+                        pine.terminal.colors.black.rgb,
+                    ),
+                },
+            });
+            _ = try registry.spawn(.{
+                WallComponent{},
+                pine.TermPositionComponent{
+                    .x = @intCast(x),
+                    .y = @intCast(height - 1 + 2),
+                },
+                pine.TermSpriteComponent{
+                    .symbol = '#',
+                    .color = pine.terminal.TermColor.fromRGB(
+                        pine.terminal.colors.white.rgb,
+                        pine.terminal.colors.black.rgb,
+                    ),
+                },
+            });
+        }
+
         // spawn the player
         _ = try registry.spawn(.{
-            Player{},
+            PlayerComponent{},
             pine.TermPositionComponent{ .x = 5, .y = 5 },
             pine.TermSpriteComponent{
                 .symbol = '@',
@@ -51,24 +123,60 @@ const PlayerMoveSystem = struct {
         };
         defer key_events.deinit();
 
-        var positions = try registry.queryComponents(.{ pine.TermPositionComponent, Player });
-        defer positions.deinit();
+        var player_positions = try registry.queryComponents(.{ pine.TermPositionComponent, PlayerComponent });
+        defer player_positions.deinit();
 
-        while (positions.next()) |position| {
-            const p = position.get(pine.TermPositionComponent).?;
+        while (player_positions.next()) |player_position| {
+            const player = player_position.get(pine.TermPositionComponent).?;
 
             while (key_events.next()) |event| {
                 switch (event) {
                     .arrow => |arrow| {
+                        var wall_positions = try registry.queryComponents(.{ pine.TermPositionComponent, WallComponent });
+                        defer wall_positions.deinit();
+
+                        var can_walk_left = true;
+                        var can_walk_right = true;
+                        var can_walk_up = true;
+                        var can_walk_down = true;
+
+                        while (wall_positions.next()) |wall_position| {
+                            const wall = wall_position.get(pine.TermPositionComponent).?;
+
+                            if (wall.x == player.x - 1) {
+                                can_walk_left = false;
+                            } else if (wall.x == player.x + 1) {
+                                can_walk_right = false;
+                            }
+
+                            if (wall.y == player.y - 1) {
+                                can_walk_up = false;
+                            } else if (wall.y == player.y + 1) {
+                                can_walk_down = false;
+                            }
+                        }
+
                         switch (arrow) {
                             .left => {
-                                if (p.x > 0) p.*.x -= 1;
+                                if (player.x > 0 and can_walk_left) {
+                                    player.*.x -= 1;
+                                }
                             },
-                            .right => p.*.x += 1,
+                            .right => {
+                                if (can_walk_right) {
+                                    player.*.x += 1;
+                                }
+                            },
                             .up => {
-                                if (p.y > 0) p.*.y -= 1;
+                                if (player.y > 0 and can_walk_up) {
+                                    player.*.y -= 1;
+                                }
                             },
-                            .down => p.*.y += 1,
+                            .down => {
+                                if (can_walk_down) {
+                                    player.*.y += 1;
+                                }
+                            },
                         }
                     },
                     else => {},
